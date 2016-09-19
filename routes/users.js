972dd5be2b36ae891ee7a1e2aabd09d27495fdb2
dclindesign <d.clin.design@gmail.com>
2016-09-19 15:41:02 -0700

    Initial Commit
    Initial Commit

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+/*
+ * UPDATE to deleteuser.
+
+router.updateuser('/updateuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToUpdate = req.params.id;
+    collection.update({ '_id' : userToUpdate }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+*/
+/*
+ * PUT to updateuser.
+ */
+router.updateuser = function(db) {
+   return function(req, res) {
+     var db = req.db;
+     var collection = db.get('userlist');
+     var userToUpdate = req.params.id;
+     collection.update({ '_id' : userToUpdate }, function(err) {
+         res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+     });
+   };
+ };
+module.exports = router;
